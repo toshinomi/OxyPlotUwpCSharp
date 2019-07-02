1b6a9bab248ff92bbb460fcab2aae5890afa0d2b
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-07-02 23:42:39 +0900

    Update

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using OxyPlot;
+using OxyPlot.Series;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
@@ -25,6 +27,45 @@ namespace OxyPlotUwpCSharp
         public MainPage()
         {
             this.InitializeComponent();
+
+            m_ViewModel = new MainViewModel();
         }
+
+        private MainViewModel m_ViewModel;
+
+        public async void OnClickFileSelect(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.VideosLibrary;
+            picker.FileTypeFilter.Add("*");
+            picker.FileTypeFilter.Add(".png");
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            var file = await picker.PickSingleFileAsync();
+            if (file == null)
+            {
+                return;
+            }
+
+            //var openFile = await file.OpenReadAsync();
+            //BitmapImage bitmap = new BitmapImage();
+            //bitmap.SetSource(openFile);
+            //this.Image.Source = bitmap;
+
+            //var swBitmap = await CreateSoftwareBitmap(file, bitmap);
+            //DrawHistgram(swBitmap);
+        }
+    }
+
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+
+        }
+
+        public PlotModel MyModel { get; private set; }
     }
 }
